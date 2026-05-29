38b397342a3a619c250c7549aac1c6d1d3606809
takei kota <ktakei2020@gmail.com>
2026-05-29 11:24:03 +0900

    環境構築完了

---
+++ src/database/factories/AttendanceCorrectionBreakFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AttendanceCorrectionBreak;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<AttendanceCorrectionBreak>
+ */
+class AttendanceCorrectionBreakFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ src/database/factories/AttendanceCorrectionRequestFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AttendanceCorrectionRequest;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<AttendanceCorrectionRequest>
+ */
+class AttendanceCorrectionRequestFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
